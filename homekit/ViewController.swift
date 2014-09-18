ca222d9f7a3285f0d24ca94c06185a765b604ebc
jumbo ken <subsubmailer@gmail.com>
2014-09-18 21:28:19 +0900

    Initial Commit

---
+++ homekit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homekit
+//
+//  Created by 羽田 健太郎 on 2014/09/18.
+//  Copyright (c) 2014年 me.jumbeeee.ken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
